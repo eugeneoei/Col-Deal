f2a579ec58a0ca63d79f9dcede7d034ccfe9d6e3
Eugene Oei <eugeneoei89@gmail.com>
2016-12-09 20:52:12 +0800

    styled login, signup and toggle between modal

--- static/index.js
+++ static/index.js
@@ -2,5 +2,14 @@ $("document").ready(function(){
   console.log("loaded");
   $('#alert').delay(5000).fadeOut(2000)
 
+  $('#sign-up-modal-toggle').on('click', function() {
+    $('#log-in-modal').modal('hide');
+    $('#sign-up-modal').modal('show');
+  });
+
+  $('#log-ing-modal-toggle').on('click', function() {
+    $('#log-in-modal').modal('show');
+    $('#sign-up-modal').modal('hide');
+  });
 
 });
